b843f9e234811874b0b453eb53281c7ce9b41b6c
Colin Paul <hilohawai23@gmail.com>
2026-03-26 19:25:20 +0530

    add to inventory functionlity on search books bar

--- force-app/main/default/lwc/bookRecommendation/bookRecommendation.js
+++ force-app/main/default/lwc/bookRecommendation/bookRecommendation.js
@@ -1,12 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import searchBooks from '@salesforce/apex/BookRecommendationService.searchBooks';
+import addBookToLibrary from '@salesforce/apex/BookRecommendationService.addBookToLibrary';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { publish, MessageContext } from 'lightning/messageService';
+import BOOK_CHANNEL from '@salesforce/messageChannel/borrowChannel__c';
 
 export default class BookRecommendation extends LightningElement {
+    columns = [
+        { label: 'Title', fieldName: 'title' },
+        { label: 'Author', fieldName: 'author' },
+        {
+            type: 'button',
+            typeAttributes: {
+                label: 'Add to Inventory',
+                name: 'add',
+                variant: 'brand'
+            }
+        }
+    ];
 
     query = '';
     books = [];
     isLoading = false;
     error;
+
+    @wire(MessageContext)
+    messageContext;
 
     handleChange(event){
         this.query = event.target.value;
@@ -29,4 +48,41 @@ export default class BookRecommendation extends LightningElement {
             this.isLoading = false;
         });
     }
+
+    handleRowAction(event){
+
+    const row = event.detail.row;
+
+    addBookToLibrary({
+        title: row.title,
+        author: row.author
+    })
+    .then(result => {
+
+        if(result === 'EXISTS'){
+            this.showToast('Info', 'Book already exists', 'info');
+        } else {
+            this.showToast('Success', 'Book added to library', 'success');
+                    publish(this.messageContext, BOOK_CHANNEL, {
+            action: 'bookAdded'
+        });
+        }
+
+    })
+    .catch(error => {
+        console.error(error);
+        this.showToast('Error', 'Something went wrong', 'error');
+    });
+}
+
+    showToast(title, message, variant){
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        })
+    );
+}
+
 }
